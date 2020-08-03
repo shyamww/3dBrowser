d3c0c1da20f2c29b5429f27ff063f068c5a3afd7
shyamww <gautam.2@iitj.ac.in>
2020-08-03 17:50:03 +0530

    Add the project first time

---
+++ Assets/Frontend/Scripts/openPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openPanel : MonoBehaviour
+{
+    public GameObject gameObject;
+    bool active;
+    public void openAndClose()
+    {
+        if(active==false)
+        {
+            gameObject.transform.gameObject.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            gameObject.transform.gameObject.SetActive(false);
+            active = false;
+        }
+    }
+}
